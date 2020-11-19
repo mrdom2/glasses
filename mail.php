a045ae9e8f8803db26d9147db722e40e5fc08f58
mrdom2 <guriev94@mail.ru>
2020-11-19 16:31:20 +0300

    glass

---
+++ mail.php
@@ -0,0 +1,55 @@
+<?php
+$field_name = $_POST['inputFIO'];
+$field_phone = $_POST['inputPhone'];
+$field_maila = $_POST['inputMail'];
+//$field_gorod = $_POST['inputGorod'];
+
+//$field_message = $_POST['topic'];
+
+$field_sender = 'Заявка. Очки.';
+
+$mail_to = 'Geo-zakaz@yandex.ru'; // Geo-zakaz@yandex.ru
+
+$subject = 'Заявка. Очки.';
+
+//if ($field_gorod != "") {
+//	$body_message  = "Заполнена контактная форма.";
+//	$body_message .= "<br>Имя: $field_name";
+//	$body_message .= "<br>Телефон: $field_phone";
+//	$body_message .= "<br>Город: $field_gorod";
+//}
+if ($field_maila == "") {
+	$body_message  = "Заполнена контактная форма.";
+	$body_message .= "<br>Имя: $field_name";
+	$body_message .= "<br>Телефон: $field_phone";
+}
+else {
+	$body_message  = "Заполнена контактная форма.";
+	$body_message .= "<br>Имя: $field_name";
+	$body_message .= "<br>Телефон: $field_phone";
+	$body_message .= "<br>E-mail: $field_maila";
+}
+
+
+$header = array(
+    "MIME-Version: 1.0",
+    "Content-Type: text/html;charset=utf-8",
+	"From: ".$field_sender
+);
+//$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, implode("\r\n", $header));
+
+if ($mail_status) { ?>
+    <script language="javascript" type="text/javascript">
+        alert('ок');
+    </script>
+<?php
+}
+else { ?>
+    <script language="javascript" type="text/javascript">
+        alert('Ошибка');
+    </script>
+<?php
+}
+?>
